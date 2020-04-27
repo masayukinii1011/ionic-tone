9d789fff89f3fdd264dd1a667808f3c844d40948
masayukinii1011 <masayukinii1011@gmail.com>
2020-04-27 23:51:29 +0900

    remove tremolo

--- src/app/synth01/synth01.page.ts
+++ src/app/synth01/synth01.page.ts
@@ -11,14 +11,11 @@ export class Synth01Page implements OnInit, AfterViewInit {
 
   synth: Tone;
 
-  tremolo: Tone;
-  tremoloFrequency = 0;
+  autoWah: Tone;
+  autoWahBaseFrequency = 400;
 
   vibrato: Tone;
   vibratoFrequency = 0;
-
-  autoWah: Tone;
-  autoWahBaseFrequency = 400;
 
   chorus: Tone;
   chorusFrequency = 0;
@@ -26,7 +23,6 @@ export class Synth01Page implements OnInit, AfterViewInit {
   delay: Tone;
   delayTime = 0;
   delayFeedback = 0;
-  delayWet = 50;
 
   reverb: Tone;
   reverbRoomSize = 0;
@@ -62,34 +58,28 @@ export class Synth01Page implements OnInit, AfterViewInit {
       },
     });
 
-    this.tremolo = new Tone.Tremolo(this.tremoloFrequency);
-    this.tremolo.start();
+    this.autoWah = new Tone.AutoWah(this.autoWahBaseFrequency);
 
     this.vibrato = new Tone.Vibrato(this.vibratoFrequency);
-
-    this.autoWah = new Tone.AutoWah(this.autoWahBaseFrequency);
 
     this.chorus = new Tone.Chorus(this.chorusFrequency);
 
     this.delay = new Tone.FeedbackDelay(this.delayTime, this.delayFeedback);
-    this.delay.wet.value = this.delayWet;
+    this.delay.wet.value = 50;
 
     this.reverb = new Tone.Freeverb(this.reverbRoomSize, this.reverbDampening);
     this.reverb.wet.value = this.reverbWet;
     this.reverb.toMaster();
 
-    this.synth.chain(this.tremolo, this.vibrato, this.autoWah, this.chorus, this.delay, this.reverb);
+    this.synth.chain(this.autoWah, this.vibrato, this.chorus, this.delay, this.reverb);
   }
 
   onChangeValue(value) {
-    if (value === this.tremoloFrequency) {
-      this.tremolo.frequency.value = value;
+    if (value === this.autoWahBaseFrequency) {
+      this.autoWah.baseFrequency = value;
     }
     if (value === this.vibratoFrequency) {
       this.vibrato.frequency.value = value;
-    }
-    if (value === this.autoWahBaseFrequency) {
-      this.autoWah.baseFrequency = value;
     }
     if (value === this.chorusFrequency) {
       this.chorus.frequency.value = value;
