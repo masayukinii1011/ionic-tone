ab6a60486958faa29038440fd94cd5f56d8c3080
masayukinii1011 <masayukinii1011@gmail.com>
2020-04-29 20:23:37 +0900

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,18 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
+  /*{
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },*/
   {
-    path: '',
-    redirectTo: 'home',
+    path: '**',
+    redirectTo: 'synth01',
     pathMatch: 'full'
   },
   {
     path: 'synth01',
-    loadChildren: () => import('./synth01/synth01.module').then( m => m.Synth01PageModule)
-  },
-  {
-    path: 'synth02',
-    loadChildren: () => import('./synth02/synth02.module').then( m => m.Synth02PageModule)
+    loadChildren: () => import('./synth01/synth01.module').then(m => m.Synth01PageModule)
   },
 ];
 
